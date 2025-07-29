6773c7567dfc3249be0f359e66d334da8e7cdf9f
Jack <155103214+Formanj-0@users.noreply.github.com>
2025-07-29 13:23:02 -0600

    Update Return_Data.py

--- src/steps/Return_Data.py
+++ src/steps/Return_Data.py
@@ -26,7 +26,6 @@ class return_data(abstract_task):
         # makes sure that the task_name is save (you can have multiple tasks of the same task)
         self.receipt['steps'][self.step_name]['task_name'] = self.task_name()
 
-
         analysis_name = self.receipt['meta_arguments']['analysis_name']
         analysis_dir = self.receipt['dirs']['analysis_dir']
         masks_dir = self.receipt['dirs']['masks_dir']
@@ -34,16 +33,12 @@ class return_data(abstract_task):
         results_dir = self.receipt['dirs']['results_dir']
         status_dir = self.receipt['dirs']['status_dir']
 
-
-
         connection_config_location = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         connection_config_location = os.path.join(connection_config_location, 'config_nas.yml')
         NASConnection(connection_config_location).write_folder_to_NAS(masks_dir, nas_dir)
         NASConnection(connection_config_location).write_folder_to_NAS(analysis_dir, nas_dir)
         NASConnection(connection_config_location).write_folder_to_NAS(results_dir, os.path.join(nas_dir, analysis_name))
         NASConnection(connection_config_location).write_folder_to_NAS(status_dir, os.path.join(nas_dir, analysis_name))
-
-
 
         # records completion. This will mark completion for luigi
         os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
