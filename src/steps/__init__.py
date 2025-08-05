cc4479cdc3229132e575fc2951bca39d8a40419c
formanj-0 <formanj@colostate.edu>
2025-08-05 09:48:08 -0600

    import match masks

--- src/steps/__init__.py
+++ src/steps/__init__.py
@@ -1,5 +1,5 @@
 from .Download_Data import download_data 
-from .Segment import segment
+from .Segment import segment, match_masks
 from .Detect_Spots import detect_spots
 from .Get_Cell_Properties import get_cell_properties
 from .Clear_Local_Data import clear_local_data
