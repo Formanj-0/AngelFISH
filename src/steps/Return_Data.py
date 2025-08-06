fbcb3455d075aed5b5d5ab351c625a368b676943
Jack <155103214+Formanj-0@users.noreply.github.com>
2025-08-06 14:45:03 -0600

    Update Return_Data.py

--- src/steps/Return_Data.py
+++ src/steps/Return_Data.py
@@ -46,7 +46,30 @@ class return_data(abstract_task):
 
 
 class copy_results(abstract_task):
-    pass
+    @classmethod
+    def task_name(cls):
+        return 'copy_results'
+    
+    def process(self):
+        start_time = time.time() 
+        results_dir = self.receipt['dirs']['results_dir']
+        nas_location = self.receipt['steps'][self.step_name]['nas_location']
+        result_name = self.receipt['steps'][self.step_name]['result_name']
+
+        connection_config_location = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        connection_config_location = os.path.join(connection_config_location, 'config_nas.yml')
+        NASConnection(connection_config_location).write_files_to_NAS(os.path.join(results_dir, result_name), nas_location)
+
+        # records completion. This will mark completion for luigi
+        os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
+        with open(self.output_path, "a") as f:
+            f.write(f"{self.step_name} completed in {time.time() - start_time:.2f} seconds\n")
+
+        return self.receipt
+
+    @staticmethod
+    def image_processing_function():
+        pass
 
 
 
