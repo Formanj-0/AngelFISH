6eaee8c2a6713c836f42bf5607e8d5723242513e
Formanj-0 <formanj@colostate.edu>
2025-08-13 13:06:45 -0600

    Add ExportImagesTask and ReconcileDataTask classes
    Introduces wrapper classes for export_images and reconcile_data functions, enabling them to be used as tasks. Updates the known_tasks list to include these new task classes.

--- src/steps/Get_Task.py
+++ src/steps/Get_Task.py
@@ -1,7 +1,36 @@
-from src.steps import segment, download_data, return_data, get_cell_properties, clear_local_data, detect_spots, match_masks, filter_csv
 
+
+from src.steps import segment, download_data, return_data, get_cell_properties, clear_local_data, detect_spots, match_masks, filter_csv, export_images, reconcile_data
+
+# Function wrappers
+class ExportImagesTask:
+    @staticmethod
+    def task_name():
+        return "export_images"
+
+    def __init__(self, receipt, step_name):
+        self.receipt = receipt
+        self.step_name = step_name
+
+    def process(self):
+        return export_images(self.receipt, self.step_name)
+
+class ReconcileDataTask:
+    @staticmethod
+    def task_name():
+        return "reconcile_data"
+
+    def __init__(self, receipt, step_name):
+        self.receipt = receipt
+        self.step_name = step_name
+
+    def process(self):
+        return reconcile_data(self.receipt, self.step_name)
+
+
+# get tasks
 known_tasks = [segment, download_data, return_data, get_cell_properties, clear_local_data, detect_spots,
-               match_masks, filter_csv]
+               match_masks, filter_csv, ExportImagesTask, ReconcileDataTask]
 
 def get_task(task_name):
     for possible_task in known_tasks:
