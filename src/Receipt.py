30b5dc60fa229a4eb91d5e2c02d1c6d571cbd2a6
Formanj-0 <formanj@colostate.edu>
2025-08-14 10:37:23 -0600

    Refactor Receipt to recalculate directories dynamically
    Introduces a recalc_dirs method to update key directory paths when meta_arguments['local_location'] changes. The __setitem__ method now intercepts changes to meta_arguments and triggers directory recalculation, improving consistency and maintainability.

--- src/Receipt.py
+++ src/Receipt.py
@@ -21,7 +21,7 @@ class Receipt(UserDict):
             with open(path, 'r') as f:
                 data = json.load(f)
 
-        if data['meta_arguments']['nas_location']: 
+        if data['meta_arguments']['nas_location'] and data['meta_arguments']['local_location'] is None: 
             # if nas-location is give we will awlays recalc the locations
             # this gives the nas location dominate 
             database_loc = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,28 +31,42 @@ class Receipt(UserDict):
             local_location = os.path.join(database_loc, name)
 
             data['meta_arguments']['local_location'] = local_location
-
-        # key directories (these will always be re-calculated)
-        data['dirs'] = {}
-        analysis_dir = os.path.join(data['meta_arguments']['local_location'], data['meta_arguments']['analysis_name'])
-        data['dirs']['analysis_dir'] = analysis_dir
-
-        results_dir = os.path.join(analysis_dir, 'results')
-        data['dirs']['results_dir'] = results_dir
-
-        status_dir = os.path.join(analysis_dir, 'status')
-        data['dirs']['status_dir'] = status_dir
-
-        mask_dir = os.path.join(local_location, 'masks')
-        data['dirs']['masks_dir'] = mask_dir
-
-        fig_dir = os.path.join(analysis_dir, 'figures')
-        data['dirs']['fig_dir'] = fig_dir
-
-
+            
         super().__init__(data)
 
+    def __setitem__(self, key, value):
+        # Intercept changes to meta_arguments['local_location']
+        if key == 'meta_arguments' and isinstance(value, dict):
+            old_local = self['meta_arguments'].get('local_location') if 'meta_arguments' in self else None
+            new_local = value.get('local_location')
+            super().__setitem__(key, value)
+            if old_local != new_local:
+                self.recalc_dirs()
+        else:
+            super().__setitem__(key, value)
 
+    def recalc_dirs(self):
+        """Recalculate key directories based on current meta_arguments."""
+        local_location = self['meta_arguments']['local_location']
+        analysis_name = self['meta_arguments']['analysis_name']
+
+        dirs = {}
+        analysis_dir = os.path.join(local_location, analysis_name)
+        dirs['analysis_dir'] = analysis_dir
+
+        results_dir = os.path.join(analysis_dir, 'results')
+        dirs['results_dir'] = results_dir
+
+        status_dir = os.path.join(analysis_dir, 'status')
+        dirs['status_dir'] = status_dir
+
+        mask_dir = os.path.join(local_location, 'masks')
+        dirs['masks_dir'] = mask_dir
+
+        fig_dir = os.path.join(analysis_dir, 'figures')
+        dirs['fig_dir'] = fig_dir
+
+        self['dirs'] = dirs
 
     def save(self, filepath):
         data_to_save = {}
