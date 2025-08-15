5f650e0c138ed77ef642afe3f2918dce600878b7
Formanj-0 <formanj@colostate.edu>
2025-08-15 12:38:23 -0600

    Export run pipeline functions in package init
    Added run_step, run_pipeline, and run_pipeline_remote imports from Run_Pipeline to src/__init__.py for easier access to pipeline execution functions.

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,4 @@ from .Receipt import Receipt
 from .Data_Loaders import close_data
 from .NASConnection import NASConnection
 from .LuigiTasks import Upload_Task, AngelFISHLuigiTask, AngelFISHWorkflow
+from .Run_Pipeline import run_step, run_pipeline, run_pipeline_remote
