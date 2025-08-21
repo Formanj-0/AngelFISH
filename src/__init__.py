d409a7dc986569dee378461e2d8c0fe99b1f11f0
formanj-0 <formanj@colostate.edu>
2025-08-21 11:32:46 -0600

    Removing Luigi for the time being

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@ from .Data_Loaders import close_data, load_data, get_data_loader
 from .Abstract_Task import abstract_task
 from .Receipt import Receipt
 from .NASConnection import NASConnection
-from .LuigiTasks import Upload_Task, AngelFISHLuigiTask, AngelFISHWorkflow
+# from .LuigiTasks import Upload_Task, AngelFISHLuigiTask, AngelFISHWorkflow
 from .Run_Pipeline import run_step, run_pipeline, run_pipeline_remote, process_path
 from .Silence import silence
